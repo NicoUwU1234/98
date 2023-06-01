7509837bc0768387968a878c396391e476bd21df
NicoUwU1234 <121208787+NicoUwU1234@users.noreply.github.com>
2023-05-31 20:18:35 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var speechrecognition = window.webkitSpeechRecognition;
 var recognition = new speechrecognition();
+
 function inicio(){
     document.getElementById("Text-box").innerHTML = "";
     recognition.start();
@@ -9,4 +10,21 @@ recognition.onresult = function(event){
     var content = event.results[0][0].transcript;
     console.log(content);
     document.getElementById("Text-box").innerHTML = content;
+    speak();
 }
+
+function speak(){
+    var synth = window.SpeechSynthesis;
+    speak_data = document.getElementById("Text-box").value;
+    var uttherThis = new SpeechSynthesisUtterance(speak_data);
+    synt.speak(uttherThis);
+    Webcam.attach(camara);
+}
+
+Webcam.set({
+    width:350,
+    height:250,
+    image_format:'png',
+    png_quality:90
+});
+camara = document.getElementByIdZ("camara");
